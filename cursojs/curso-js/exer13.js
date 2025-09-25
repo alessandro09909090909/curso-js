04081ee1b0c535d5873d294ccbd40c19e7fc37e0
alessandro09909090909 <alessandro.santos.cardoso@escola.pr.gov.br>
2025-09-25 12:42:04 -0300

    Add files via upload

---
+++ cursojs/curso-js/exer13.js
@@ -0,0 +1,13 @@
+// Criando um Array
+
+// 1. Declarar a variável com pelo menos 4 elementos
+const filmesFavoritos = ["Matrix", "Interestelar", "O Senhor dos Anéis", "Homem-Aranha"];
+
+// 2. Acessar e imprimir o segundo elemento
+console.log("Segundo elemento da lista:", filmesFavoritos[1]);
+
+// 3. Adicionar um novo elemento ao final do array
+filmesFavoritos.push("Vingadores");
+
+// 4. Imprimir o array completo
+console.log("Lista completa de filmes:", filmesFavoritos);
